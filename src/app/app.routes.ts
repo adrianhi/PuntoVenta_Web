8196297153be2add66af8a79ab7f45708e5184c4
Adrian Hidalgo <hidalgobeltreadrian@gmail.com>
2024-05-11 13:03:40 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ClientComponent } from './core/Pages/client/client.component';
+import { ProductsComponent } from './core/Pages/products/products.component';
+import { SaleComponent } from './core/Pages/sale/sale.component';
+import { StartComponent } from './core/Pages/start/start.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StartComponent },
+  { path: 'client', component: ClientComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'sale', component: SaleComponent },
+];
